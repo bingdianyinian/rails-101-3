320f889595a0928d129a10513fd220a35beae3d0
Huang Xun <bingdianyinian@gmail.com>
2017-01-21 17:23:33 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :groups
-         has_many :posts
+  has_many :groups
+  has_many :posts
+  has_many :relationships
+  has_many :participated_groups, :through => :group_relationship, :source => :group
 end
